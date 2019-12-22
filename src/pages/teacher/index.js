2e4462774f291b939806e3a3f255d83d4dd800cb
yan-ck <yan_ck1987@163.com>
2019-12-22 22:46:38 +0800

    dev

--- src/pages/teacher/index.js
+++ src/pages/teacher/index.js
@@ -1,28 +1,47 @@
 import React from 'react';
-import { Tabs } from 'antd';
+import { Tabs, Descriptions } from 'antd';
 import router from 'umi/router';
 import styles from './index.less';
 
 const { TabPane } = Tabs;
 
-class Teacher extends React.Component{
-  onTabClick = (e)=>{
-    if(e==="基础资料"){
+class Teacher extends React.Component {
+  onTabClick = e => {
+    if (e === '基础资料') {
       router.push('/teacher');
-    }else if(e==="课程管理"){
+    } else if (e === '课程管理') {
       router.push('/teacher/courseManage');
     }
+  };
+  componentWillUnmount() {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'teacher/save',
+      payload: {},
+    });
   }
-  render(){
+  render() {
     return (
       <div className={styles.box}>
         <Tabs defaultActiveKey="基础资料" onTabClick={this.onTabClick}>
-          <TabPane tab='基础资料' key="基础资料"></TabPane>
-          <TabPane tab='课程管理' key="课程管理"></TabPane>
+          <TabPane tab="基础资料" key="基础资料">
+            <Descriptions title="用户信息">
+              <Descriptions.Item label="学号">Zhou Maomao</Descriptions.Item>
+              <Descriptions.Item label="姓名">1810000000</Descriptions.Item>
+              <Descriptions.Item label="性别">Hangzhou, Zhejiang</Descriptions.Item>
+              <Descriptions.Item label="电话">empty</Descriptions.Item>
+              <Descriptions.Item label="出生日期">
+                No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
+              </Descriptions.Item>
+              <Descriptions.Item label="地址">Zhou Maomao</Descriptions.Item>
+              <Descriptions.Item label="院系">1810000000</Descriptions.Item>
+              <Descriptions.Item label="邮箱">Hangzhou, Zhejiang</Descriptions.Item>
+            </Descriptions>
+          </TabPane>
+          <TabPane tab="课程管理" key="课程管理"></TabPane>
         </Tabs>
       </div>
-    )
+    );
   }
-  
 }
 export default Teacher;
